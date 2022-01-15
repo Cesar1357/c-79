270930c6ea5a7c7b3f064ddc8367deb3f7eef0d8
Cesar1357 <85760661+Cesar1357@users.noreply.github.com>
2022-01-15 17:49:17 -0600

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
 import * as Font from "expo-font";
 
-import BottomTabNavigator from "./components/BottomTabNavigator";
+import BottomTabNavigator from "./BottomTabNavigator";
 
 export default class App extends Component {
   constructor() {
