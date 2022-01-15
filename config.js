f35cee2eaefd0a7a0dd7a51593c5ceb2fee90577
Cesar1357 <85760661+Cesar1357@users.noreply.github.com>
2022-01-15 17:48:54 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+import { collection, doc, setDoc } from "firebase/firestore";
+//Pega tu configuración de firebase aquí
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBMF6-RUbnXL9YLC8PyutwipLGBaiP8zI8",
+    authDomain: "c-79-b92ee.firebaseapp.com",
+    projectId: "c-79-b92ee",
+    storageBucket: "c-79-b92ee.appspot.com",
+    messagingSenderId: "405162912282",
+    appId: "1:405162912282:web:84f4f75860fdaceaaa7fa3"
+  };
+
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
+
+
